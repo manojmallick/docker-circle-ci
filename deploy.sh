f3e00710b186edc7533d21d4a53c416ffdb62f1b
manojmallick <manoj.mallick079@gmail.com>
2020-01-05 05:48:41 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ git clone https://github.com/manojmallick/docker-circle-ci.git
 sudo curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 cd docker-circle-ci
-sudo docker-compose up -d
+docker-compose up -d
 exit
